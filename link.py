5efe6f4608a8267c426e637f4e5281684156e79e
geolocal78 <elclashhar91929@gmail.com>
2024-08-30 19:17:49 -0300

    link.py

---
+++ link.py
@@ -0,0 +1,15 @@
+import webbrowser
+ 
+  url = "https://minosis.com"  # Reemplaza con la URL
+  webbrowser.open(url)
+  print("https://minosis.com")
+  print("https://www.indec.gob.ar")
+  print("https://www.afip.gob.ar")
+  print("https://www.bcra.gob.ar")
+  print("https://www.economia.gob.ar")
+ print("https://www.trabajo.gob.ar")
+ print("https://www.argentina.gob.ar/salud")
+ print("https://www.argentina.gob.ar/superintendencia-de-seguros-de-
+ print("https://www.cammesa.com")
+ print("http://www.uca.edu.ar")
+ print("https://www.dpn.gob.ar")
