d0d34c741e17b730b55b14f2d1a0f344d30ad452
geolocal78 <elclashhar91929@gmail.com>
2024-08-30 20:01:12 -0300

    link.py

--- link.py
+++ link.py
@@ -1,15 +1,13 @@
-import webbrowser
- 
-  url = "https://minosis.com"  # Reemplaza con la URL
-  webbrowser.open(url)
-  print("https://minosis.com")
-  print("https://www.indec.gob.ar")
-  print("https://www.afip.gob.ar")
-  print("https://www.bcra.gob.ar")
-  print("https://www.economia.gob.ar")
- print("https://www.trabajo.gob.ar")
- print("https://www.argentina.gob.ar/salud")
- print("https://www.argentina.gob.ar/superintendencia-de-seguros-de-
- print("https://www.cammesa.com")
- print("http://www.uca.edu.ar")
- print("https://www.dpn.gob.ar")
+import webbrowser                                                                                  2                                                                                                    3 url = "https://minosis.com"  # Reemplaza con la URL
+webbrowser.open(url)
+print("https://minosis.com")
+print("https://www.indec.gob.ar")
+print("https://www.afip.gob.ar")
+print("https://www.bcra.gob.ar")
+print("https://www.economia.gob.ar")
+print("https://www.trabajo.gob.ar")
+print("https://www.argentina.gob.ar/salud")
+print("https://www.argentina.gob.ar/superintendencia-de-seguros-de-la-nacion")
+print("https://www.cammesa.com")
+print("http://www.uca.edu.ar")
+print("https://www.dpn.gob.ar")
